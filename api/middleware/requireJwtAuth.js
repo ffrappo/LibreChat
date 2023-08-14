ebd720f3e5256adc617b63dd58edc79d9e7e15aa
BlitzWrecker <wot.dragon0407@gmail.com>
2023-08-14 10:56:21 -0600

    JwtAuth assigns false to req.user if no logged in user

--- api/middleware/requireJwtAuth.js
+++ api/middleware/requireJwtAuth.js
@@ -1,5 +1,20 @@
 const passport = require('passport');
 
-const requireJwtAuth = passport.authenticate('jwt', { session: false });
+const requireJwtAuth = (req, res, next) => {
+  passport.authenticate('jwt', { session: false }, (err, user) => {
+    if (err) {
+      console.log(err);
+    }
+    if (!user) {
+      console.log('(requireJwtAuth): No user');
+    }
+    // Assigns a value to req.user no matter what
+    // Known possible values:
+    //   false: no logged in user
+    //   user object: logged in user
+    req.user = user;
+    next();
+  })(req, res, next);
+}
 
 module.exports = requireJwtAuth;
