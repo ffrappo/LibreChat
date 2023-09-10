a0c4678448393c78d180ed0fddeda53a99466f7d
Shi Jin <jinzishuai@gmail.com>
2023-09-09 20:11:59 -0600

    initial commit to viewCount: 5 per read

--- api/server/routes/messages.js
+++ api/server/routes/messages.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const { getMessages, likeMessage } = require('../../models/Message');
+const { increaseConvoViewCount } = require('../../models/Conversation');
 const requireJwtAuth = require('../../middleware/requireJwtAuth');
 
 router.get('/:conversationId', requireJwtAuth, async (req, res) => {
   const { conversationId } = req.params;
+  await increaseConvoViewCount(conversationId); //Added for https://github.com/aitok-ai/LibreChatInternal/issues/95
   res.status(200).send(await getMessages({ conversationId }));
 });
 
 router.post('/like', async (req, res) => {
-  const { messageId, isLiked } = req.body; 
+  const { messageId, isLiked } = req.body;
 
   try {
     const dbResponse = await likeMessage(messageId, isLiked);
