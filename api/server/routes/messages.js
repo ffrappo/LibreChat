91e332a4969409ef2d4167725f09e77bf399217d
Shi Jin <jinzishuai@gmail.com>
2023-09-12 22:05:47 -0600

    don't increase viewcount if viewed by  same user

--- api/server/routes/messages.js
+++ api/server/routes/messages.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const { getMessages, likeMessage } = require('../../models/Message');
-const { increaseConvoViewCount } = require('../../models/Conversation');
+const { increaseConvoViewCount, getSharedConvo } = require('../../models/Conversation');
 const requireJwtAuth = require('../../middleware/requireJwtAuth');
 
 router.get('/:conversationId', requireJwtAuth, async (req, res) => {
   const { conversationId } = req.params;
-  await increaseConvoViewCount(conversationId); //Added for https://github.com/aitok-ai/LibreChatInternal/issues/95
+  const convo = await getSharedConvo(conversationId);
+  if (convo.user !== req.user.id) {
+    //increment view count only if the user is not the owner of the conversation
+    await increaseConvoViewCount(conversationId, req.user.id); //Added for https://github.com/aitok-ai/LibreChatInternal/issues/95
+  }
   res.status(200).send(await getMessages({ conversationId }));
 });
 
